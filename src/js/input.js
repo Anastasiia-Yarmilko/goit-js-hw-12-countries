c7ef05c0f032bd0b0cf112ccbbbf35521540b80d
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-03-21 00:43:57 +0200

    Внесла изменения в css

--- src/js/input.js
+++ src/js/input.js
@@ -18,32 +18,33 @@ function displayCountries(event) {
     refsSearchResults.innerHTML = '';
     const countrySearchName = event.target.value;
     fetchCountries(countrySearchName)
-    .then(results => {
-        if (results.length > 10) {
-            alert({
-                text: 'Too many matches found. Please enter a more specific query!',
-                type: 'error',
-                delay: 1000,
-                stack: new Stack({
-            dir1: 'up',
-        }),
-    });
-}
-if (results.length >= 2 && results.length <= 10) {
-    refsSearchResults.insertAdjacentHTML(
-        'beforeend',
-        createListCountriesTemplate(results),
-        );
-      }
-      if (results.length === 1) {
-        refsSearchResults.insertAdjacentHTML(
-            'beforeend',
-            createCountryPropertiesTemplate(results),
-            );
-        }
-    })
-    .catch(console.log);
-}
+        .then(results => {
+            if (results.length > 10) {
+                alert({
+                    text: 'Too many matches found. Please enter a more specific query!',
+                    type: 'error',
+                    delay: 1000,
+                    stack: new Stack({
+                        dir1: 'up',
+                    }),
+                });
+            }
+            if (results.length >= 2 && results.length <= 10) {
+                refsSearchResults.insertAdjacentHTML(
+                    'beforeend',
+                    createListCountriesTemplate(results),
+                );
+            };
+
+            if (results.length === 1) {
+                refsSearchResults.insertAdjacentHTML(
+                    'beforeend',
+                    createCountryPropertiesTemplate(results),
+                    );
+            };
+        })
+        .catch(console.log)
+};
 
 function createListCountriesTemplate(results) {
   const template =
@@ -62,11 +63,12 @@ function createCountryPropertiesTemplate(results) {
         return acc;
     }, '');
     const templateCountry = `<h2 class='country-name'>${results[0].name}</h2>
-    <div class='wrapper'><div class='country-properties'>
+    <div class='country-card'>
+    <img width='500px' src="${results[0].flag}" alt="flag"></img>
+    <div class='country-properties'>
     <p><span class='country-property'>Capital: </span>${results[0].capital}</p>
     <p><span class='country-property'>Population: </span>${results[0].population}</p>
-    <h3 class='country-property'>Languages:</h3><ul>${languageDisplay}</ul></div>
-    <img height='400px' src="${results[0].flag}" alt="flag"></img></div>`;
+    <h3 class='country-property'>Languages:</h3><ul>${languageDisplay}</ul></div></div>`;
     return templateCountry;
 };
 
