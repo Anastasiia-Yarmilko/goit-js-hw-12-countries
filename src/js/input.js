05e7c7cf39791b71ba9246b78681ce5d0dcb8c9f
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-03-20 21:32:46 +0200

    Оформила отображение карточки одной страны

--- src/js/input.js
+++ src/js/input.js
@@ -54,5 +54,18 @@ function createListOfCountries(result) {
 };
     
 function createCountryCard(result) {
+    const language = result[0].languages.reduce((acc, item) => {
+        acc += `<li>${item.name}</li>`;
+        return acc;
+    }, '');
 
+    const countryCard = `<h2 class='country-name'>${data[0].name}</h2>
+        <div class='country-card'><div class='country-properties'>
+        <p><span class='country-property'>Capital: </span>${data[0].capital}</p>
+        <p><span class='country-property'>Population: </span>${data[0].population}</p>
+        <h3 class='country-property'>Languages:</h3><ul>${language}</ul></div>
+        <img height='500px' src="${data[0].flag}" alt="flag of coutnry"></img></div>`;
+    return countryCard;
 };
+
+refs.input.addEventListener('input', debounce(handleInput, 500));
